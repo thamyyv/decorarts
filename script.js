892334876d466185e85ebaf71d18232dd0739ff4
Thamy <tvgagt@gmail.com>
2024-07-09 21:54:30 -0300

    Inicio da responsividade

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
- // Função para exibir a página especificada
- function showPage(pageId) {
-    var pages = document.querySelectorAll('.container');
-    pages.forEach(function(page) {
-        if (page.id === pageId) {
-            page.style.display = 'block';
-        } else {
-            page.style.display = 'none';
-        }
-    });
-}
-
-// Evento para lidar com o envio de imagem
-document.getElementById('addImageForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var fileInput = document.getElementById('imageInput');
-    var files = fileInput.files;
-    if (files.length > 0) {
-        var image = files[0];
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var imageElement = document.createElement('img');
-            imageElement.src = e.target.result;
-            var gallery = document.querySelector('.image-grid');
-            var item = document.createElement('div');
-            item.classList.add('image-item');
-            item.appendChild(imageElement);
-            gallery.appendChild(item);
-        }
-        reader.readAsDataURL(image);
+function toggleNavbar() {
+    let menuMobile = document.querySelector('.navbar-nav');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
     }
-});
+}
