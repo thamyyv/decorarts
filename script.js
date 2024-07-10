123fe43257556ba68b6c6604c3c34f47778c46fe
Thamy <tvgagt@gmail.com>
2024-07-09 23:39:55 -0300

    Finalização da responsavidade para mobile

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function toggleNavbar() {
-    let menuMobile = document.querySelector('.navbar-nav');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-    } else {
-        menuMobile.classList.add('open');
-    }
+  let menuMobile = document.querySelector(".navbar-nav");
+  menuMobile.classList.toggle("open");
 }
